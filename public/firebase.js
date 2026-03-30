2145875f947645391869ecb08f73b443ff4a2543
Nigel Smith <nigel.nds.smith@gmail.com>
2026-03-30 10:26:29 -0600

    added api

--- public/firebase.js
+++ public/firebase.js
@@ -7,13 +7,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCU5KpQa9T7lFHq4AdnHcYugJAwwIZrjbQ",
   authDomain: "cit170-activity-votes.firebaseapp.com",
   projectId: "cit170-activity-votes",
-  storageBucket: "cit170-activity-votes.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "cit170-activity-votes.firebasestorage.app",
+  messagingSenderId: "771325751641",
+  appId: "1:771325751641:web:8822d42a3541cccfb1dc48",
+  measurementId: "G-YW7CSN76JM"
 };
 
 const app = initializeApp(firebaseConfig);
